af72c412323c75b1e8574066239e2778e9978fa8
Martin Imre <martinimre25@gmail.com>
2025-03-21 13:02:53 +0100

    feat: Add basic frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Sudoku from './components/Sudoku.vue'
+</script>
+
+<template>
+  <Sudoku
+    :data="[1, 2, 3, 4, 5, 6, 7, 8, 9,
+      1, 2, 3, 0, 0, 0, 0, 0, 0,
+      2, 2, 3, 0, 0, 0, 0, 0, 0,
+      3, 2, 3, 0, 0, 0, 0, 0, 0,
+      4, 2, 3, 0, 0, 0, 0, 0, 0,
+      5, 2, 3, 0, 0, 0, 0, 0, 0,
+      6, 2, 3, 0, 0, 0, 0, 0, 0,
+      7, 2, 3, 0, 0, 0, 0, 0, 0,
+      8, 2, 3, 0, 0, 0, 0, 0, 0]
+    "
+  />
+</template>
+
+<style scoped></style>
